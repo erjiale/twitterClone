3f7f1024e1edc890d3297e4d10a09efd3cec33b7
Jiale Qiu <jiale98chinoguay@gmail.com>
2019-11-08 02:48:18 -0500

    finalized part 2 of twitter clone

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -15,7 +15,7 @@ class HomeTableViewController: UITableViewController {
     
     let myRefreshControl = UIRefreshControl()
 
-
+    
     // view Loads!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +23,14 @@ class HomeTableViewController: UITableViewController {
         
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        
     }
 
+    // function so that it loads the tweet i just tweeted.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
     
     @objc func loadTweets() {
        
@@ -100,6 +106,10 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
